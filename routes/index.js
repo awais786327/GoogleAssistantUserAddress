80b78f3eaea5cfdbb4e25fa3822f423e7ccc79dd
awais786327 <awais3227443@gmail.com>
2019-12-01 20:01:59 +0500

    init

--- routes/index.js
+++ routes/index.js
@@ -55,7 +55,7 @@ app.intent('Types Of Things Can Manage', (conv, {material}) => {
   console.log(`\nTypes Of Things Can Manage\n`);
   const userStorage = localStorage('userStorage');
 
-  console.log(userStorage);
+  console.log(userStorage, material);
 
   if (!(userStorage && userStorage.address)) {
     return askForLocation(conv, material);
