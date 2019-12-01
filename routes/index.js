99d767867e707531ffe8f1472f0e47caae8af512
awais786327 <awais3227443@gmail.com>
2019-12-01 21:08:11 +0500

    init

--- routes/index.js
+++ routes/index.js
@@ -94,16 +94,15 @@ app.intent('Location Permission Granted', (conv, params, permissionGranted) => {
 
   const userStorage = localStorage('userStorage');
 
+  const {city, coordinates, formattedAddress} = conv.device.location;
+
   if (userStorage.requestedPermissionType === 'DEVICE_COARSE_LOCATION') {
-    const {city, formattedAddress} = conv.device.location;
-    userStorage['address'] = formattedAddress;
+    userStorage['address'] = formattedAddress || 'Address';
     userStorage['location'] = city;
   }
 
   if (userStorage.requestedPermissionType === 'DEVICE_PRECISE_LOCATION') {
-    const {coordinates, formattedAddress} = conv.device.location;
-    console.log(conv);
-    userStorage['address'] = formattedAddress;
+    userStorage['address'] = formattedAddress || 'Address';
     userStorage['location'] = {
       latitude: coordinates.latitude,
       longitude: coordinates.longitude,
